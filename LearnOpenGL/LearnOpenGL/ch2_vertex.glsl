30c192ba654a69772e320ed033eba9f948b7aa85
FurryMonster <2876231904@qq.com>
2025-03-12 17:38:25 +0800

    textures

--- LearnOpenGL/LearnOpenGL/ch2_vertex.glsl
+++ LearnOpenGL/LearnOpenGL/ch2_vertex.glsl
@@ -1,13 +1,16 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 uniform float loc_offset_x;
 uniform float loc_offset_y;
 out vec3 ourColor;
+out vec2 TexCoord;
 
 void main()
 {
     gl_Position = vec4(aPos.x + loc_offset_x,-aPos.y+loc_offset_y,aPos.z, 1.0);
     ourColor = aColor;
+    TexCoord = aTexCoord;
 }
